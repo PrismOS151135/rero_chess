6739a9846348a2ea679fcf9a5a423de76881ef8c
MrZ_26 <1046101471@qq.com>
2025-01-04 02:41:07 +0800

    修一个shader过不了非电脑平台编译

--- assets/shader/lighter.hlsl
+++ assets/shader/lighter.hlsl
@@ -2,7 +2,7 @@ extern float k;
 
 vec4 effect(vec4 color, sampler2D tex, vec2 texCoord, vec2 scrCoord) {
     return vec4(
-        mix(vec3(color),vec3(1),k),
+        mix(vec3(color), vec3(1.0), k),
         texture2D(tex, texCoord).a*color.a
     );
 }
